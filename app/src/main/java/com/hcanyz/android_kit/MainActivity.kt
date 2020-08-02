1c61ceb351bbb4f13ec3edf43d28722b47708d7d
yan.kong <hcanyz@163.com>
2020-08-02 11:08:20 +0800

    feat(deps): 结构优化

--- app/src/main/java/com/hcanyz/android_kit/MainActivity.kt
+++ app/src/main/java/com/hcanyz/android_kit/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hcanyz.android_kit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
