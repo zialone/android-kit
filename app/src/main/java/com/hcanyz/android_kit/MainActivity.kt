2718dc3410e4ea2d3be378edd41f1729ba5b31ff
hcanyz <hcanyz@163.com>
2020-09-17 16:06:51 +0800

    feat(widget_views_stateview): stateview init

--- app/src/main/java/com/hcanyz/android_kit/MainActivity.kt
+++ app/src/main/java/com/hcanyz/android_kit/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.genzon.reception.pad.widget.views.stateview.customizeStateEmpty
 import com.hcanyz.android_kit.feature.template.api.IApiTemplate
 import com.hcanyz.android_kit.feature.template.api.IDiTemplateProvided
 import com.hcanyz.android_kit.vendor.config.BuildConfig
@@ -11,6 +12,7 @@ import com.hcanyz.android_kit.vendor.config.IZConfig
 import com.hcanyz.android_kit.vendor.log.ZLog
 import com.hcanyz.android_kit.widget.res.ThemeSwitchTransitionActivity
 import com.hcanyz.android_kit.widget.storage.uniqueKeyUntilUninstalled
+import com.kennyc.view.MultiStateView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Retrofit
@@ -34,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hello(view: View) {
+
         ZLog.dTime(TAG, "hello")
 
         IApiTemplate.api().hello(view.context)
@@ -58,5 +61,15 @@ class MainActivity : AppCompatActivity() {
         izConfig.canLog = !izConfig.canLog
 
         tv_logSwitch.text = "logSwitch: ${izConfig.canLog}"
+    }
+
+    fun stateView(view: View) {
+        msv_test.customizeStateEmpty("$title")
+
+        msv_test.viewState = MultiStateView.ViewState.LOADING
+
+        view.postDelayed({
+            msv_test.viewState = MultiStateView.ViewState.CONTENT
+        }, 1500)
     }
 }
---
+++ widgets/widget_views_stateview/src/main/java/com/genzon/reception/pad/widget/views/stateview/StateView.kt
@@ -0,0 +1,19 @@
+package com.genzon.reception.pad.widget.views.stateview
+
+import android.view.View
+import android.widget.TextView
+import com.kennyc.view.MultiStateView
+
+fun MultiStateView.customizeStateEmpty(emptyTip: String) {
+    findViewById<TextView>(R.id.tv_stateview_empty_tip).text = emptyTip
+}
+
+fun MultiStateView.customizeStateError(
+    errorTip: String? = null,
+    retryOnClickListener: View.OnClickListener? = null
+) {
+    errorTip?.let {
+        findViewById<TextView>(R.id.tv_stateview_error_tip).text = it
+    }
+    findViewById<TextView>(R.id.tv_stateview_error_retry).setOnClickListener(retryOnClickListener)
+}
