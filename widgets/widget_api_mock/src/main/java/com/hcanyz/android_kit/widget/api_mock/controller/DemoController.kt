ad65c1dee972d593446b2cb7aaa372ea7499a208
栉风 <hcanyz@163.com>
2021-06-08 11:54:09 +0800

    chore: ~

--- widgets/widget_api_mock/src/main/java/com/hcanyz/android_kit/widget/api_mock/controller/DemoController.kt
+++ widgets/widget_api_mock/src/main/java/com/hcanyz/android_kit/widget/api_mock/controller/DemoController.kt
@@ -1,13 +1,20 @@
 package com.hcanyz.android_kit.widget.api_mock.controller
 
 import com.yanzhenjie.andserver.annotation.GetMapping
+import com.yanzhenjie.andserver.annotation.PathVariable
+import com.yanzhenjie.andserver.annotation.RequestParam
 import com.yanzhenjie.andserver.annotation.RestController
 
 @RestController
 class DemoController {
 
-    @GetMapping("/demo/1")
-    fun login(): String {
-        return "DemoController /demo/1 successful."
+    @GetMapping("/demo/{id}")
+    fun demo(@PathVariable("id") id: String): String {
+        return "DemoController /demo/${id} successful."
+    }
+
+    @GetMapping("/user/login")
+    fun loginByPwd(@RequestParam("username") username: String): String {
+        return "{\"code\":0,\"username\":\"${username}\"}"
     }
 }
